e644f58323a39663e76fd2961c514ce75ca07245
LidiaBa <lida.babuschckin@yandex.ru>
2026-05-11 01:03:57 +0300

    Запуск TomCat

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,35 @@
 package org.example;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import lombok.extern.java.Log;
+
+import java.io.File;
+import java.util.logging.Level;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Настройка Tomcat
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.setBaseDir("temp");
+        String contextPath = "/";
+        String docBase = new File(".").getAbsolutePath();
+        Context ctx = tomcat.addContext(contextPath, docBase);
+        //Tomcat.addServelet(ctx, "myServelet", new MyServelet());
+        //ctx.addServeletMappingdecoded("/hello", "myServelet");
+        try {
+            System.out.println("Запуск TomCat на порту 8080");
+            tomcat.start();
+            // блокируем основной поток
+            // ждем пока серсер не скажет остановиться
+            tomcat.getServer().await();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        } catch (LifecycleException ex) {
+            ex.printStackTrace();
         }
     }
 }
